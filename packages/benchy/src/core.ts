8f6b70f9741890f0edcc4c06a424350b355148b8
AidanTilgner <aidantilgner02@gmail.com>
2024-11-20 23:10:45 -0800

    feat: switched to starlight for docs

--- packages/benchy/src/core.ts
+++ packages/benchy/src/core.ts
@@ -1,1 +1,47 @@
-export const add = (a: number, b: number) => a + b;
+type ITestConfiguration = {
+  name: string;
+};
+
+export class Test {
+  _name: string;
+
+  constructor({ name }: ITestConfiguration) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+}
+
+type IDomainConfiguration = {
+  name: string;
+};
+
+export class Domain {
+  private _name: string;
+  private _tests: Record<string, Test> = {};
+
+  constructor({ name }: IDomainConfiguration) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get tests() {
+    return this._tests;
+  }
+}
+
+export class Proctor {
+  domains: Record<string, Domain> = {};
+
+  constructor() {}
+
+  registerDomain(domain: Domain) {
+    this.domains[domain.name] = domain;
+    console.info("Domain registered");
+  }
+}
